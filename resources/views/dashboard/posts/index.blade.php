d91820ca061a811c5e1d182bf5decea18c8e1b46
Adi Saputra <adi.juve22@gmail.com>
2022-01-09 12:15:47 +0700

    Fitur Update & Delete

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -4,7 +4,7 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">My Posts</h1>
 </div>
-<div class="table-responsive col-lg-5">
+<div class="table-responsive col-lg-11">
   @if (session()->has('success'))
   <div class="alert alert-success alert-dismissible fade show" role="alert">
     {{session('success')}}
@@ -25,13 +25,17 @@
       </thead>
       <tbody>
         @foreach ( $posts as $post )
-        <tr>
+        <tr> 
           <td>{{ $loop->iteration }}</td>
           <td>{{ $post->title }}</td>
           <td>{{ $post->category->name }}</td>
           <td><a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
-            <a href="/dashboard/posts/{{ $post->id }}" class="badge bg-warning"><span data-feather="edit"></span></a>
-            <a href="/dashboard/posts/{{ $post->id }}" class="badge bg-danger"><span data-feather="x-circle"></span></a>
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+            @method('delete')
+            @csrf
+            <button class="badge bg-danger border-0" onclick="return confirm('Are you sure??')"><span data-feather="x-circle"></button>
+            </form>
           </td>
         </tr>
         @endforeach
