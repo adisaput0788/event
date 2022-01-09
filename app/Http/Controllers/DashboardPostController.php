930a2faf3bb61983ce38faf3a1666cc65d5f8ae2
Adi Saputra <adi.juve22@gmail.com>
2022-01-09 20:27:19 +0700

    Update & Delete Image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 
 class DashboardPostController extends Controller
@@ -108,6 +109,7 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ];
 
@@ -116,6 +118,13 @@ class DashboardPostController extends Controller
         }
 
         $validatedData = $request->validate($rules); 
+
+        if($request->file('image')){
+            if($request->oldImage){
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
 
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 100);
@@ -134,7 +143,10 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        Post::destroy($post->id);
+        if($post->image){
+            Storage::delete($post->image);
+        }
+        Post::destroy($post->id); 
 
         return redirect('/dashboard/posts')->with('success', 'Post has been deleted');
     }
