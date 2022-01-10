e3937b6bfa6ac5226e9e5039abaa6e0aa03d75a5
Adi Saputra <adi.juve22@gmail.com>
2022-01-10 13:21:15 +0700

    Fitur Auth Admin & Category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,11 +7,11 @@
     <div class="row justify-content-center mb-5">
         <div class="col-md-8">
             <h2 class="mb-3">{{ $post->title }}</h2>
-    
+
     <p> By <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}"class="text-decoration-none">{{ $post->category->name }}</a></p>
-    @if ($post->image)
+         @if ($post->image)
         <div style="max-height: 350px; overflow:hidden;">
-        <img src="{{ asset('storage/' . $post->image) }}" class="img-fluid mt-3" alt="{{ 
+        <img src="{{ asset('storage/' . $post->image) }}" class="img-fluid mt-3" alt="{{
         $post->category->name }}">
         </div>
         @else
@@ -21,13 +21,13 @@
     <p>{!! $post->body !!}</p>
     </article>
 
-<a href="/blog" class="text-decoration-none">Back To Posts</a>
+<a href="/posts" class="text-decoration-none">Back To Posts</a>
         </div>
     </div>
 </div>
-  
 
-    
-    
+
+
+
 @endsection
 
